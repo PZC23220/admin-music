385d35460f23ca772d04bb9c99ac3212e972f7a1
zhicui <zhicuipang@newsjet-inc.com>
2017-12-11 16:42:58 +0800

    去掉多余页面

--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,9 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 //用户相关
-import addList from '@/components/songList/addList'
-import hotList from '@/components/songList/hotList'
-import opularityList from '@/components/songList/opularityList'
+// import addList from '@/components/songList/addList'
+// import hotList from '@/components/songList/hotList'
+// import opularityList from '@/components/songList/opularityList'
 import allList from '@/components/songList/allList'
 
 
@@ -17,9 +17,9 @@ export default new Router({
   routes: [
     {path: '/', redirect: '/login'},
     {path: '/login', component: login},
-    {path: '/addList', component: addList},
-    {path: '/hotList', component: hotList},
-    {path: '/opularityList', component: opularityList},
+    // {path: '/addList', component: addList},
+    // {path: '/hotList', component: hotList},
+    // {path: '/opularityList', component: opularityList},
     {path: '/allList', component: allList},
   ]
 })
