2d8b6e120c64df6c874133e4f09b813c655faaeb
zhicui <zhicuipang@newsjet-inc.com>
2017-12-11 15:09:54 +0800

    修改接口文件

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,20 @@ module.exports = {
     {
       name: "music",
       script: "bin/www",
+      out_file : "/logs/music_out.log",
+      error_file : "/logs/music_error.log",
+      env: {
+        "NODE_ENV": "development"
+      },
+      env_dev: {
+        "NODE_ENV": "product"
+      }
+    },
+    {
+      name: "opt",
+      script: "/home/admin-elementUI/bin/www",
+      out_file : "/logs/opt_out.log",
+      error_file : "/logs/opt_error.log",
       env: {
         "NODE_ENV": "development"
       },
