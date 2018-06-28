9384323fda9e9ef8d22fc7fddb1c3c4d789963a6
zhicui <zhicuipang@newsjet-inc.com>
2018-06-28 10:25:06 +0800

    添加提示

--- src/components/publish/app_info.vue
+++ src/components/publish/app_info.vue
@@ -59,7 +59,8 @@
             <i class="el-icon-upload"></i>
             <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
             <div class="el-upload__tip"  style="color:#f00" slot="tip">因为安装包有点大，所以上传时间会久一点。请耐心等待！不要中途放弃！！
-            <br><b>⚠️如果重复上传将会替换上一次上传的包</b>
+            <br><b>⚠️注意：上传文件过程中进度显示100%并不代表成功了！！！<br>
+⚠️当你看到100%消失了变成了上面的包链接地址也变了size也有了，那么恭喜你！传成功了！</b>
             <br><b>⚠️上传的包后缀是.apk，这个我希望你真的知道！！</b></div>
           </el-upload>
         </div>
